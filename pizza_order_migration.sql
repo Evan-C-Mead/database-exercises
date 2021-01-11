d350275448b6533ea52f09ba041a2c08f951dc4f
Evan Mead <evan.c.mead@gmail.com>
2021-01-11 13:55:59 -0600

    small changes to pizza orders data types

--- pizza_order_migration.sql
+++ pizza_order_migration.sql
@@ -4,16 +4,16 @@ DROP TABLE IF EXISTS pizza_orders;
 
 CREATE TABLE IF NOT EXISTS pizza_orders (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(25) NOT NULL,
+    name VARCHAR(255) NOT NULL DEFAULT 'Unknown',
+    phone_number VARCHAR(25) NOT NULL DEFAULT 'Unknown',
     email VARCHAR(255),
-    address VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL DEFAULT 'Unknown',
     pizza_size INT NOT NULL,
     total_cost DECIMAL(10, 2) NOT NULL,
     description VARCHAR(255),
     is_delivered TINYINT default 0,
     created_at DATE,
-    updated_at DATE,
+    updated_at INT UNSIGNED,
     PRIMARY KEY (id),
     UNIQUE KEY phone_number (phone_number)
 );
