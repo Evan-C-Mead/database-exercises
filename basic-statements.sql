9f315e08d47bfb64b9a67de9422b4b31c83b630d
Evan Mead <evan.c.mead@gmail.com>
2021-01-11 16:22:02 -0600

    update exercises complete

---
+++ basic-statements.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+INSERT INTO album(artist, name, release_date) VALUES('Eric Prydz', 'Opus', 2012);
+INSERT INTO album(artist, name, release_date) VALUES('The Chemical Brothers', 'Born in the Echos', 2015);
+INSERT INTO album(artist, name, release_date) VALUES('The Glitch Mob', 'Drink The Sea', 2010);
+INSERT INTO album(artist, name, release_date) VALUES('Daft Punk', 'Homework', 1997);
+INSERT INTO album(artist, name, release_date) VALUES('Daft Punk', 'Discovery', 2001);
+
+INSERT INTO album(artist, name, release_date)
+VALUES('deadmau5', 'Random Album Title', 2008);
+      ('deadmau5', 'For Lack of a Better Name', 2008);
+      ('deadmau5', '4x4=12', 2008);
+      ('deadmau5', '> album title goes here <', 2008);
+      ('deadmau5', 'W:/2016ALBUM/', 2008);
+
