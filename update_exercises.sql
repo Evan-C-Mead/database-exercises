ae870fe073e1725bdb165bea4780acee6e6b602a
Evan Mead <evan.c.mead@gmail.com>
2021-01-12 09:17:36 -0600

    update exercise complete

---
+++ update_exercises.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+SELECT name AS 'All Albums' FROM album;
+
+SELECT name AS 'Before 1980' FROM album WHERE release_date < 1980;
+
+SELECT name AS 'Michael Jackson' FROM album WHERE artist = 'Michael Jackson';
+
+UPDATE album SET sales = sales * 10;
+SELECT * FROM album;
+
+UPDATE album SET release_date = 1800 WHERE release_date < 1980;
+SELECT name, release_date FROM album WHERE release_date < 1980;
+
+UPDATE album SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+SELECT artist, name FROM album WHERE artist = 'Peter Jackson';
