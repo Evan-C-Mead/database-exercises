558464a3d5bc12fc08deba5502eb7014c2be91f6
Evan Mead <evan.c.mead@gmail.com>
2021-01-12 15:33:33 -0600

    second problem set complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,20 +2,30 @@ use employees;
 
 select first_name, last_name
 from employees
-where first_name in ('Irena', 'Vidya', 'Maya');
+where gender = 'M'
+and (first_name in ('Irena', 'Vidya')
+   or first_name = 'Maya');
 
 select first_name, last_name
 from employees
-where last_name like 'E%';
+where last_name like 'E%'
+or last_name like '%e';
+
+# select first_name, last_name
+# from employees
+# where last_name like 'E%e';
 
 select first_name, last_name
 from employees
-where hire_date like '199%';
+where last_name like 'E%'
+  and (last_name like '%e');
 
 select first_name, last_name
 from employees
-where birth_date like '%-12-25';
+where hire_date like '199%'
+  and (birth_date like '%-12-25');
 
 select  first_name, last_name
 from employees
-where last_name like '%q%';
+where last_name like '%q%'
+  and (employees.last_name not like '%qu%');
