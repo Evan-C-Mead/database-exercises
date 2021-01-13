c6b13bb7a8d4653f7e96aab5761579516ef344df
Evan Mead <evan.c.mead@gmail.com>
2021-01-13 11:24:11 -0600

    swapped display columns

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -4,8 +4,8 @@ select concat(first_name, ' ', last_name) as 'Employee names that start and end 
 from employees
 where last_name like 'E%e';
 
-select datediff(current_date(), hire_date) as 'Days worked at Company',
-concat(first_name, ' ', last_name) as 'Employee'
+select concat(first_name, ' ', last_name) as 'Employee',
+       datediff(current_date(), hire_date) as 'Days worked at Company'
 from employees
 where hire_date like '199%'
   and (birth_date like '%-12-25')
