676245c42863c75cffda4edd590cbab0843fccbf
Evan Mead <evan.c.mead@gmail.com>
2021-01-13 09:02:08 -0600

    gitignore changed

--- where_exercises.sql
+++ where_exercises.sql
@@ -22,6 +22,10 @@ where last_name like 'E%'
 
 select first_name, last_name
 from employees
+where hire_date like '199%';
+
+select first_name, last_name
+from employees
 where hire_date like '199%'
   and (birth_date like '%-12-25');
 
