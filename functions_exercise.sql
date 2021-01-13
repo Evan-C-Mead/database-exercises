c44357998db07daa36f25dc2958a150db35ce773
Evan Mead <evan.c.mead@gmail.com>
2021-01-13 11:19:23 -0600

    days worked problem complete - exercise complete

--- functions_exercise.sql
+++ functions_exercise.sql
@@ -4,7 +4,8 @@ select concat(first_name, ' ', last_name) as 'Employee names that start and end 
 from employees
 where last_name like 'E%e';
 
-select first_name, last_name
+select datediff(current_date(), hire_date) as 'Days worked at Company',
+concat(first_name, ' ', last_name) as 'Employee'
 from employees
 where hire_date like '199%'
   and (birth_date like '%-12-25')
