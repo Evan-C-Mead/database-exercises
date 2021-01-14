b9d05a8c33c8de40bf321a9dc192b6ea6d7c1e4d
Evan Mead <evan.c.mead@gmail.com>
2021-01-14 10:25:58 -0600

    aliases exercise complete

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,2 +1,13 @@
 use employees;
 
+select concat(last_name, ', ', first_name) as full_name
+from employees
+limit 10;
+
+select concat(last_name, ', ', first_name) as full_name, birth_date as DOB
+from employees
+limit 10;
+
+select concat(emp_no, ' - ', last_name, ', ', first_name) as employee_number_and_full_name, birth_date as DOB
+from employees
+limit 10;
