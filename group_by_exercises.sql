444fca46e0cd74a2902d45038c3463fd5ce26672
Evan Mead <evan.c.mead@gmail.com>
2021-01-14 10:14:38 -0600

    group by exercise complete

---
+++ group_by_exercises.sql
@@ -0,0 +1,32 @@
+use employees;
+
+select distinct title
+from titles;
+
+select last_name
+from employees
+where last_name like 'E%e'
+group by last_name;
+
+select last_name, first_name
+from employees
+where last_name like 'E%e'
+group by last_name, first_name;
+
+select last_name
+from employees
+where last_name like '%q%'
+  and last_name not like '%qu%'
+group by last_name;
+
+select last_name, count(last_name)
+from employees
+where last_name like 'E%e'
+  or last_name like '%q%'
+  and last_name not like '%qu%'
+group by last_name;
+
+select count(gender), gender
+from employees
+where first_name in ('Irena', 'Vidya', 'Maya')
+group by gender;
