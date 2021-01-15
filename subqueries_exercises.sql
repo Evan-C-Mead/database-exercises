c64e27b899f9f235e70db754c0839cc1dfd604be
Evan Mead <evan.c.mead@gmail.com>
2021-01-15 14:07:40 -0600

    bonuses complete

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,14 +1,14 @@
 use employees;
 
-select *
+select first_name, last_name
 from employees
 where hire_date = (
     select hire_date
     from employees
-    where emp_no = '101010'
+    where emp_no = 101010
     );
 
-select *
+select title
 from titles
 where emp_no in (
     select emp_no
@@ -22,5 +22,28 @@ where emp_no in (
     select emp_no
     from dept_manager
     where dept_manager.to_date > now()
-      and employees.gender = 'F'
+    )
+      and employees.gender = 'F';
+
+# BONUS 1
+select dept_name
+from departments
+where dept_no in (
+    select dept_manager.dept_no
+    from dept_manager
+    where emp_no in (
+        select emp_no
+        from employees
+        where employees.gender = 'F'
+        )
+      and dept_manager.to_date > now()
+    );
+
+# BONUS 2
+select first_name, last_name
+from employees
+where emp_no in (
+    select emp_no
+    from salaries
+    where salary = '158220'
     );
